698d6871e41ba3e60f06a95c083043c923d6d16a
Muhamad Usri Yusron <muhamad.usri@mhs.itenas.ac.id>
2025-12-02 22:10:32 +0700

    afoewewf

--- app/Models/Ketersediaan.php
+++ app/Models/Ketersediaan.php
@@ -4,12 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-<<<<<<< HEAD
-=======
 use App\Models\Detail;
 
->>>>>>> cd26f31254b49eb9a31483249892a3e50d31da7e
 class Ketersediaan extends Model
 {
     use HasFactory;
